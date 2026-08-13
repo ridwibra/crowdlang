9643b4ac055dea46b85978c703b2b7cba162cc7c
ridwan <ridwibra@gmail.com>
2026-08-13 01:40:36 -0400

    fix header

--- components/shared/HeaderNav.tsx
+++ components/shared/HeaderNav.tsx
@@ -11,7 +11,7 @@ export default function HeaderNav() {
     <nav
       className="
         flex items-center gap-10 text-sm font-medium
-        absolute left-1/2 -translate-x-1/2 mr-2
+        absolute left-1/2 -translate-x-1/2
       "
     >
       {/* HOME */}
@@ -47,7 +47,7 @@ export default function HeaderNav() {
       {/* MAP */}
       <Link
         href="/map"
-        className={`flex items-center gap-2 transition
+        className={`flex items-center gap-2 transition mr-3
           ${
             pathname.startsWith("/map")
               ? "text-blue-600 dark:text-blue-400 font-semibold"
